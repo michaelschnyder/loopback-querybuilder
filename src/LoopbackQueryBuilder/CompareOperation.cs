158024ee46b6061ac1f8d3cca571d6d749745807
Michael Schnyder <michael.schnyder@zuehlke.com>
2018-08-13 10:25:42 +0800

    moved to separate file

---
+++ src/LoopbackQueryBuilder/CompareOperation.cs
@@ -0,0 +1,28 @@
+﻿namespace LoopbackQueryBuilder
+{
+    internal class CompareOperation : EqualityOperation
+    {
+        private readonly ComparisionMode _mode;
+
+        public CompareOperation(ComparisionMode mode)
+        {
+            _mode = mode;
+        }
+
+        public override string ToString()
+        {
+            var rawValue = string.Empty;
+            var operationString = string.Empty;
+
+            if (_mode == ComparisionMode.Contains)
+            {
+                operationString = "like";
+                rawValue = $"%{Value}%";
+            }
+
+            var saveValue = !this.IsSaveValue ? $"'{rawValue}'" : rawValue;
+
+            return $"{{ {ColumnName}: {{ '{operationString}': {saveValue} }} }}";
+        }
+    }
+}
--- src/LoopbackQueryBuilder/Translator.cs
+++ src/LoopbackQueryBuilder/Translator.cs
@@ -107,32 +107,6 @@ namespace LoopbackQueryBuilder
         }
     }
 
-    internal class CompareOperation : EqualityOperation
-    {
-        private readonly ComparisionMode _mode;
-
-        public CompareOperation(ComparisionMode mode)
-        {
-            _mode = mode;
-        }
-
-        public override string ToString()
-        {
-            var rawValue = string.Empty;
-            var operationString = string.Empty;
-
-            if ((_mode == ComparisionMode.Contains))
-            {
-                operationString = "like";
-                rawValue = $"%{Value}%";
-            }
-
-            var saveValue = !this.IsSaveValue ? $"'{rawValue}'" : rawValue;
-
-            return $"{{ {ColumnName}: {{ '{operationString}': {saveValue} }} }}";
-        }
-    }
-
     public enum ComparisionMode
     {
         Contains
